a562ff54d4fef32452deb4db01d61800a5a135bf
megumi <megumi@192.168.13.24>
2024-01-19 14:00:18 +0900

    Progate lesson1-9

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -1,10 +1,21 @@
 package practice;
 
-public class JavaPracticeClass {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello World!!");
-	}
-
+class JavaPracticeClass {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("名前：");
+    String firstName = scanner.next();
+    System.out.print("名字：");
+    String lastName = scanner.next();
+    System.out.print("年齢：");
+    int age = scanner.nextInt();
+    System.out.print("身長(m)：");
+    double height = scanner.nextDouble();
+    System.out.print("体重(kg)：");
+    double weight = scanner.nextDouble();
+    
+    JavaSubClass.printData(firstName, lastName, age, height, weight);
+  }
 }
