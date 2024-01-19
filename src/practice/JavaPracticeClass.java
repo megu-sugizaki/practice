06a1dbd785f576248da4a77bbf1d0a6f135495b6
megumi <megusugizaki@gmail.com>
2024-01-19 16:52:24 +0900

    Progate lesson1-9 10/11 fixed

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -7,9 +7,10 @@ class JavaPracticeClass {
 	    Scanner scanner = new Scanner(System.in);
 	    System.out.print("何人分の情報を入力しますか:");
 	    int amount = scanner.nextInt();
+	    
 	    int maxAge = 0;
 	    
-	    for (int i = 0; i <= amount; i++) {
+	    for (int i = 0; i < amount; i++) {
 	      System.out.println(i + 1 + "人目");
 	      System.out.print("名前：");
 	      String firstName = scanner.next();
@@ -17,17 +18,15 @@ class JavaPracticeClass {
 	      String lastName = scanner.next();
 	      System.out.print("年齢：");
 	      int age = scanner.nextInt();
+	      if (age > maxAge) {
+	        maxAge = age;
+	      }
 	      System.out.print("身長(m)：");
 	      double height = scanner.nextDouble();
 	      System.out.print("体重(kg)：");
 	      double weight = scanner.nextDouble();
 	      JavaSubClass.printData(firstName, lastName, age, height, weight);
-	      
-	      if (age > maxAge) {
-	        maxAge = age;
-	      }
-	      
-	      System.out.println("最高年齢は" + maxAge + "です");
 	    }
+	    System.out.println("最高年齢は" + maxAge + "です");
 	  }
 }
