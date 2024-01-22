f0ae92b9496d50fb0dc67c8e4ad30126de9212ee
megumi <megusugizaki@gmail.com>
2024-01-22 18:03:35 +0900

    Progate lesson4-13

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -3,9 +3,13 @@ package practice;
 class JavaPracticeClass {
 	public static void main(String[] args) {
 	    JavaSubClass person1 = new JavaSubClass("Kate", "Jones", 27, 1.6, 50.0);
-
-	    System.out.println(person1.fullName());
-	    System.out.println(person1.age);
-	    System.out.println(person1.bmi());
+	    person1.printData();
+	    
+	    JavaSubClass person2 = new JavaSubClass("John", "Smith", 65, 1.75, 80.0);
+	    person2.printData();
+	    
+	    // 「合計◯◯人です」と出力してください
+	    System.out.println("合計" + JavaSubClass.count + "人です");
+	    
 	  }
 }
