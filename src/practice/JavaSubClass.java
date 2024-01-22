b680ccc52e23bf6ef5459a981236af7f58da24a7
megumi <megusugizaki@gmail.com>
2024-01-22 17:43:37 +0900

    Progate lesson4-12

--- src/practice/JavaSubClass.java
+++ src/practice/JavaSubClass.java
@@ -1,28 +1,33 @@
 package practice;
 
 class JavaSubClass {
-	public String firstName;
+	  public String firstName;
 	  public String lastName;
 	  public int age;
 	  public double height;
 	  public double weight;
 
-	 JavaSubClass(String firstName, String lastName, int age, double height, double weight) {
+	  JavaSubClass(String firstName, String lastName, int age, double height, double weight) {
 	    this.firstName = firstName;
 	    this.lastName = lastName;
 	    this.age = age;
 	    this.height = height;
 	    this.weight = weight;
 	  }
-	  
-	  // fullNameメソッドを定義してください
-	  public String fullName(){
-	    return this.firstName + " " + this.lastName; 
+
+	  public String fullName() {
+	    return this.firstName + " " + this.lastName;
+	  }
+
+	  public double bmi() {
+	    return this.weight / this.height / this.height;
 	  }
 	  
-	  // bmiメソッドを定義してください
-	  public double bmi(){
-	    return this.weight / this.height / this.height;
+	  // printDataメソッドを定義してください
+	  public void printData(){
+	    System.out.println("私の名前は" + this.fullName() + "です");
+	    System.out.println("年齢は" + this.age + "歳です");
+	    System.out.println("BMIは" + Math.round(this.bmi()) + "です");
 	  }
 	  
 	}
