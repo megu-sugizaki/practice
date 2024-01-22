da18277d272af7f50aad112edf2921adfe17275d
megumi <megusugizaki@gmail.com>
2024-01-22 15:35:09 +0900

    Progate lesson4-6

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -3,22 +3,11 @@ package practice;
 class JavaPracticeClass {
 	public static void main(String[] args) {
 	    JavaSubClass person1 = new JavaSubClass();
-	    person1.hello();
-	    
-	    // person1のインスタンスフィールドnameに「Kate Jones」をセットしてください
 	    person1.name = "Kate Jones";
-	    
-	    // person1のインスタンスフィールドnameの値を出力してください
-	    System.out.println(person1.name);
+	    person1.hello();
 
 	    JavaSubClass person2 = new JavaSubClass();
-	    person2.hello();
-	    
-	    // person2のインスタンスフィールドnameに「John Christopher Smith」をセットしてください
 	    person2.name = "John Christopher Smith";
-	    
-	    // person2のインスタンスフィールドnameの値を出力してください
-	    System.out.println(person2.name);
-	    
+	    person2.hello();
 	  }
 }
--- src/practice/JavaSubClass.java
+++ src/practice/JavaSubClass.java
@@ -5,7 +5,8 @@ class JavaSubClass {
 	  public String name;
 
 	  public void hello() {
-	    System.out.println("こんにちは");
+	    // thisを用いて、「こんにちは、私は◯◯です」で出力するように以下を書き換えてください
+	    System.out.println("こんにちは、私は" + this.name + "です" );
 	  }
 	  
 	}
