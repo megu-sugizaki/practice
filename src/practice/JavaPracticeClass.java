c987f9a6c9b6b332620ef4696a0a82b952bdde2c
megumi <megusugizaki@gmail.com>
2024-01-22 17:29:04 +0900

    Progate lesson4-11

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -3,11 +3,9 @@ package practice;
 class JavaPracticeClass {
 	public static void main(String[] args) {
 	    JavaSubClass person1 = new JavaSubClass("Kate", "Jones", 27, 1.6, 50.0);
-	    
-	    System.out.println(person1.firstName);
-	    System.out.println(person1.lastName);
+
+	    System.out.println(person1.fullName());
 	    System.out.println(person1.age);
-	    System.out.println(person1.height);
-	    System.out.println(person1.weight);
+	    System.out.println(person1.bmi());
 	  }
 }
--- src/practice/JavaSubClass.java
+++ src/practice/JavaSubClass.java
@@ -1,20 +1,28 @@
 package practice;
 
 class JavaSubClass {
-	// 以下にインスタンスフィールドを定義してください
-	  public String firstName;
+	public String firstName;
 	  public String lastName;
 	  public int age;
 	  public double height;
 	  public double weight;
 
-	  // 以下にコンストラクタを定義し、インスタンスフィールドに値をセットしてください
-	  JavaSubClass(String firstName, String lastName, int age, double height, double weight){
+	 JavaSubClass(String firstName, String lastName, int age, double height, double weight) {
 	    this.firstName = firstName;
 	    this.lastName = lastName;
 	    this.age = age;
 	    this.height = height;
 	    this.weight = weight;
+	  }
+	  
+	  // fullNameメソッドを定義してください
+	  public String fullName(){
+	    return this.firstName + " " + this.lastName; 
+	  }
+	  
+	  // bmiメソッドを定義してください
+	  public double bmi(){
+	    return this.weight / this.height / this.height;
 	  }
 	  
 	}
