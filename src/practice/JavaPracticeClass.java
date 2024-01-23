2855a3cd57d66c85c29cb9c7c8dc5f1da885b176
megumi <megusugizaki@gmail.com>
2024-01-23 16:01:55 +0900

    Progate Java dojo2-4

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -2,7 +2,7 @@ package practice;
 
 class JavaPracticeClass {
 	public static void main(String[] args) {
-	    JavaSubClass bicycle1 = new JavaSubClass("ビアンキ", "緑");
+	    JavaSubClass bicycle1 = new JavaSubClass("ビアンキ", "緑", 0);
 	    System.out.println("【自転車の情報】");
 	    bicycle1.printData();
 	  }
--- src/practice/JavaSubClass.java
+++ src/practice/JavaSubClass.java
@@ -1,28 +1,31 @@
 package practice;
 
 class JavaSubClass {
-	  // インスタンスフィールド
+	 // インスタンスフィールド
 	  private String name;
 	  private String color;
+	  private int distance;
 	  
 	  // コンストラクション
-	  JavaSubClass(String name, String color){
+	  JavaSubClass(String name, String color, int distance){
 	   this.name = name;
 	   this.color = color;
+	   this.distance = distance;
 	  }
 	 
 	// publicでインスタンスフィールドを使用できるように
-	  // public String getName(){
-	  // return this.name;
-	  // }
+	  public int getDistance(){
+	  return this.distance;
+	  }
 	 
-	  // public void setName(String name){
-	  // this.name = name;
-	  // }
+	  public void setDistance(int distance){
+	  this.distance = distance;
+	  }
 	  
 	  // 最終的に出力したい情報群
 	  public void printData(){
 	    System.out.println("名前：" + this.name);
 	    System.out.println("色：" + this.color);
+	    System.out.println("走行距離：" + this.distance + "km");
 	  }
 	}
