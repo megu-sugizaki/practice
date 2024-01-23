df7677562aaab822d3ed0379e694d16a20a3ead1
megumi <megusugizaki@gmail.com>
2024-01-23 15:56:33 +0900

    Progate Java dojo2-3

--- src/practice/JavaSubClass.java
+++ src/practice/JavaSubClass.java
@@ -1,21 +1,28 @@
 package practice;
 
 class JavaSubClass {
+	  // インスタンスフィールド
 	  private String name;
+	  private String color;
 	  
-	  JavaSubClass(String name){
+	  // コンストラクション
+	  JavaSubClass(String name, String color){
 	   this.name = name;
+	   this.color = color;
 	  }
 	 
-	  public String getName(){
-	   return this.name;
-	  }
+	// publicでインスタンスフィールドを使用できるように
+	  // public String getName(){
+	  // return this.name;
+	  // }
 	 
 	  // public void setName(String name){
 	  // this.name = name;
 	  // }
 	  
+	  // 最終的に出力したい情報群
 	  public void printData(){
 	    System.out.println("名前：" + this.name);
+	    System.out.println("色：" + this.color);
 	  }
 	}
