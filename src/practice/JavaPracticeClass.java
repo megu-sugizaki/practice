40bc07ecceced3053058de2d809593ec412ca0c2
megumi <megusugizaki@gmail.com>
2024-01-28 08:42:16 +0900

    Progate Java 5-4

--- src/practice/JavaPracticeClass.java
+++ src/practice/JavaPracticeClass.java
@@ -1,25 +1,26 @@
 package practice;
+import java.util.Scanner;
 
 class JavaPracticeClass {
 	public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
 	    JavaThirdClass car = new JavaThirdClass();
-	    // setNameメソッドを用いて、carの名前を「フェラーリ」にしてください
 	    car.setName("フェラーリ");
-	   
-	    
-	    // setColorメソッドを用いて、carの色を「赤」にしてください
 	    car.setColor("赤");
-	    
 	    JavaSubClass bicycle = new JavaSubClass();
-	    // setNameメソッドを用いて、bicycleの名前を「ビアンキ」にしてください
 	    bicycle.setName("ビアンキ");
-	    
-	    
-	    // setColorメソッドを用いて、bicycleの色を「緑」にしてください
 	    bicycle.setColor("緑");
-	    
+
 	    System.out.println("【車の情報】");
 	    car.printData();
+	    // getFuelメソッドを用いて「ガソリン量：◯◯L」と出力してください
+	    System.out.println("ガソリン量："  + car.getFuel() + "L");
+	    
+	    System.out.println("-----------------");
+	    System.out.print("給油する量を入力してください：");
+	    int litre = scanner.nextInt();
+	    // chargeメソッドを呼び出して、引数にlitreを渡してください
+	    car.charge(litre);
 
 	    System.out.println("=================");
 	    System.out.println("【自転車の情報】");
